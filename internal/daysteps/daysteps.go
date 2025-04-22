bae76d4784bb80f5d0357f8dfc13d9c11cbb9036
Aleksei Velichkin <mail@avelichkin.ru>
2025-04-22 17:57:51 +0300

    feat: implement the logic of the parsePackage() function

--- internal/daysteps/daysteps.go
+++ internal/daysteps/daysteps.go
@@ -1,6 +1,10 @@
 package daysteps
 
 import (
+	"errors"
+	"fmt"
+	"strconv"
+	"strings"
 	"time"
 )
 
@@ -11,8 +15,31 @@ const (
 	mInKm = 1000
 )
 
+var (
+	ErrInvalidArguments = errors.New("invalid arguments")
+	ErrInvalidFormat    = errors.New("invalid format")
+)
+
 func parsePackage(data string) (int, time.Duration, error) {
-	// TODO: реализовать функцию
+	var steps int
+	var duration time.Duration
+
+	trainingRecord := strings.Split(data, ",")
+	if len(trainingRecord) != 2 {
+		return 0, 0, ErrInvalidArguments
+	}
+
+	steps, err := strconv.Atoi(trainingRecord[0])
+	if err != nil {
+		return 0, 0, ErrInvalidFormat
+	}
+
+	duration, err = time.ParseDuration(trainingRecord[1])
+	if err != nil {
+		return 0, 0, ErrInvalidFormat
+	}
+
+	return steps, duration, nil
 }
 
 func DayActionInfo(data string, weight, height float64) string {
